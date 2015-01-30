0d773d23c40d464ef0d54bc7a74f571e137c5cf9
Luke A. Guest <laguest@archeia.com>
2015-01-30 18:12:22 +0000

    Added GNAT_Util library to build.

---
+++ source/gnat_util/gnat_util_dummies.c
@@ -0,0 +1,39 @@
+/****************************************************************************
+ *                                                                          *
+ *                        GNAT COMPILER COMPONENTS                          *
+ *                                                                          *
+ *                   G N A T _ U T I L _ D U M M I E S                      *
+ *                                                                          *
+ *                    Copyright (C) 2010-2012, AdaCore                      *
+ *                                                                          *
+ * GNAT is free software;  you can  redistribute it  and/or modify it under *
+ * terms of the  GNU General Public License as published  by the Free Soft- *
+ * ware  Foundation;  either version 3,  or (at your option) any later ver- *
+ * sion.  GNAT is distributed in the hope that it will be useful, but WITH- *
+ * OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE.                                     *
+ *                                                                          *
+ *                                                                          * 
+ *                                                                          * 
+ *                                                                          * 
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License and    *
+ * a copy of the GCC Runtime Library Exception along with this program;     *
+ * see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    *
+ * <http://www.gnu.org/licenses/>.                                          *
+ *                                                                          *
+ * GNAT was originally developed  by the GNAT team at  New York University. *
+ * Extensive contributions were provided by Ada Core Technologies Inc.      *
+ *                                                                          *
+ ****************************************************************************/
+
+void
+set_std_prefix (char *path, int len)
+{
+}
+
+char *
+update_path (char *path, char *key)
+{
+  return path;
+}
