e7fbffba2363b2111b1167fdaafc6a6750b8c2ed
Luke A. Guest <laguest@archeia.com>
2019-11-05 14:05:54 +0000

    Add Python pre-requisites. Not packaged into an archive right now.

--- includes/python.inc.sh
+++ includes/python.inc.sh
@@ -83,3 +83,91 @@ function python()
 
     echo "  >> Python bootstrap ($3) Installed"
 }
+
+function install_python_packages()
+{
+	local TASK_COUNT_TOTAL=8
+ 	VER="$build_type/$1"
+	DIRS="python-pkgs"
+	LOGPRE=$LOG/$VER
+	OBD=$BLD/$VER
+
+    echo "  >> Creating Directories (if needed)..."
+
+    cd $BLD
+    for d in $DIRS; do
+        if [ ! -d $VER/$d ]; then
+            mkdir -p $VER/$d
+        fi
+    done
+
+    cd $OBD/$DIRS
+
+    echo "  >> Downloading required Python packages..."
+
+    if [ ! -f get-pip.py ]; then
+        echo "  >> [1/$TASK_COUNT_TOTAL] Downloading get-pip.py..."
+
+        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py &> $LOGPRE/$DIRS-pip-download.txt
+
+        check_error_exit
+    fi
+
+    if [ ! -f .installed-pip ]; then
+        echo "  >> [2/$TASK_COUNT_TOTAL] Packaging PIP..."
+
+        python2.7 get-pip.py &> $LOGPRE/$DIRS-pip-install.txt
+
+        check_error .installed-pip
+    fi
+
+    if [ ! -f .installed-mako ]; then
+        echo "  >> [3/$TASK_COUNT_TOTAL] Packaging Mako..."
+
+        pip2.7 install mako &> $LOGPRE/$DIRS-mako-install.txt
+
+        check_error .installed-mako
+    fi
+
+    if [ ! -f .installed-pyyaml ]; then
+        echo "  >> [4/$TASK_COUNT_TOTAL] Packaging PyYAML..."
+
+        pip2.7 install pyyaml &> $LOGPRE/$DIRS-pyyaml-install.txt
+
+        check_error .installed-pyyaml
+    fi
+
+    if [ ! -f .installed-enum ]; then
+        echo "  >> [5/$TASK_COUNT_TOTAL] Packaging Enum..."
+
+        pip2.7 install enum &> $LOGPRE/$DIRS-enum-install.txt
+
+        check_error .installed-enum
+    fi
+
+    if [ ! -f .installed-enum34 ]; then
+        echo "  >> [6/$TASK_COUNT_TOTAL] Packaging Enum34..."
+
+        pip2.7 install enum34 &> $LOGPRE/$DIRS-enum34-install.txt
+
+        check_error .installed-enum34
+    fi
+
+    if [ ! -f .installed-funcy ]; then
+        echo "  >> [7/$TASK_COUNT_TOTAL] Packaging Funcy..."
+
+        pip2.7 install funcy &> $LOGPRE/$DIRS-funcy-install.txt
+
+        check_error .installed-funcy
+    fi
+
+    if [ ! -f .installed-docutils ]; then
+        echo "  >> [8/$TASK_COUNT_TOTAL] Packaging DocUtils..."
+
+        pip2.7 install docutils &> $LOGPRE/$DIRS-docutils-install.txt
+
+        check_error .installed-docutils
+    fi
+
+    echo "  >> Python packages Installed"
+}
